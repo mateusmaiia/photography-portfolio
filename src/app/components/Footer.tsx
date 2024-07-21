fa74edc15a0e5e673441c6faad715ecd20942f91
Maia <matteusmaia889@gmail.com>
2024-07-21 19:39:33 -0300

    feat: footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="relative mt-10 text-white h-[90px] text-lg uppercase font-medium flex items-center justify-center">
+      <p>Photography Portfolio</p>
+    </footer>
+  )
+}
+
+export default Footer
